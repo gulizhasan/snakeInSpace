d27700a0e97929e918c039158976b6f9aafdeee3
Guliz Hasan <btrmguliz@gmail.com>
2024-01-12 17:07:38 +0200

    implements continuous movement and food generation with some error handling

--- procedural.py
+++ procedural.py
@@ -1,66 +1,80 @@
 import random
 import curses
 
-
-def create_food(snake, window):
+def create_food(snake, window, sh, sw):
     food = None
     while food is None:
-        food = [random.randint(1, curses.COLS - 2),
-                random.randint(1, curses.LINES - 2)]
+        food = [random.randint(1, sh - 2), random.randint(1, sw - 2)]
         if food in snake:
             food = None
-    window.addch(food[1], food[0], curses.ACS_PI)
+    window.addch(food[0], food[1], curses.ACS_PI)
     return food
 
-
 def snake_game(stdscr):
-    curses.curs_set(0)
-    stdscr.timeout(100)
+    curses.curs_set(0)  # Hide the cursor
+    sh, sw = stdscr.getmaxyx()  # Get the window's height and width
+    w = curses.newwin(sh, sw, 0, 0)  # Create a new window for the game
+    w.keypad(1)  # Enable keypad input
+    w.timeout(100)  # Set the screen timeout
 
-    sh, sw = stdscr.getmaxyx()
-    w = curses.newwin(sh, sw, 0, 0)
-    w.keypad(1)
+    # Initialize the snake's initial position and direction
+    snake_x = sw//4
+    snake_y = sh//2
+    snake = [
+        [snake_y, snake_x],
+        [snake_y, snake_x-1],
+        [snake_y, snake_x-2]
+    ]
+    food = create_food(snake, w, sh, sw)
+    w.addch(food[0], food[1], curses.ACS_PI)
 
-    snake = [[4, 4], [4, 3], [4, 2]]
-    food = create_food(snake, w)
+    key = curses.KEY_RIGHT  # Start by moving the snake to the right
 
-    key = curses.KEY_RIGHT
+    score = 0
 
     while True:
         next_key = w.getch()
         key = key if next_key == -1 else next_key
 
-        new_head = [snake[0][0], snake[0][1]]
-
+        # Determine the new head of the snake based on the key direction
         if key == curses.KEY_DOWN:
-            new_head[1] += 1
+            new_head = [snake[0][0]+1, snake[0][1]]
         elif key == curses.KEY_UP:
-            new_head[1] -= 1
+            new_head = [snake[0][0]-1, snake[0][1]]
         elif key == curses.KEY_LEFT:
-            new_head[0] -= 1
+            new_head = [snake[0][0], snake[0][1]-1]
         elif key == curses.KEY_RIGHT:
-            new_head[0] += 1
+            new_head = [snake[0][0], snake[0][1]+1]
 
+        # Insert the new head of the snake
         snake.insert(0, new_head)
 
-        if (
-            new_head[0] in [0, sw] or
-            new_head[1] in [0, sh] or
-            new_head in snake[1:]
-        ):
+        # Check if snake has eaten the food
+        if snake[0] == food:
+            score += 1
+            food = create_food(snake, w, sh, sw)
+            w.addch(food[0], food[1], curses.ACS_PI)
+        else:
+            # Move the snake by removing the tail
+            tail = snake.pop()
+            w.addch(tail[0], tail[1], ' ')
+
+        # Check for collisions with the border
+        if snake[0][0] in [1, sh-2] or snake[0][1]  in [1, sw-2] or snake[0] in snake[1:]:
             break
 
-        if new_head == food:
-            food = create_food(snake, w)
-        else:
-            tail = snake.pop()
-            w.addch(tail[1], tail[0], ' ')
+        try:
+            # Draw the new head of the snake
+            w.addch(snake[0][0], snake[0][1], curses.ACS_CKBOARD)
+        except:
+            break  # In case the snake tries to go outside the bounds
 
-        w.addch(snake[0][1], snake[0][0], curses.ACS_CKBOARD)
+    stdscr.addstr(sh//2, sw//2, "Game Over! Score: {}".format(score))
+    stdscr.refresh()
+    stdscr.getch()
 
-    w.addstr(sh // 2, sw // 2, "Game Over", curses.A_BOLD)
-    w.refresh()
-    w.getch()
+def main():
+    curses.wrapper(snake_game)
 
-
-curses.wrapper(snake_game)
+if __name__ == "__main__":
+    main()
